e36dedddad1d618b243fad9efbf11f32eafcc1f0
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-02-28 13:15:53 -0600

    Security ready!
    #3

--- control/forms.py
+++ control/forms.py
@@ -7,21 +7,22 @@
 #       autor: Javier Sanchez Toledano
 #       fecha: 25 de enero de 2023
 
-from dal import autocomplete
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Div, HTML, Field, Button
-from crispy_forms.bootstrap import InlineRadios, Tab, TabHolder, FormActions
-from django.core.exceptions import NON_FIELD_ERRORS
+
 from django import forms
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
-from control.models import Pusinex, Municipio, Localidad, Seccion, Revision
+from control.models import Revision
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class PUSINEXForm(forms.ModelForm):
+    seccion = forms.IntegerField()
+    localidad = forms.IntegerField()
+    f_act = forms.DateField()
+    hojas = forms.IntegerField()
+    archivo = forms.FileField()
+    observaciones = forms.CharField(widget=forms.Textarea, required=False)
+
     class Meta:
         exclude = ('pusinex', 'user', )
         model = Revision
